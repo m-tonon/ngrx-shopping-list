d7d46f995d88b0a32b4ff3ec3392bdf0410a28df
mtonon <math.tonon@gmail.com>
2024-03-21 06:11:15 -0300

    refact: declare interface for ingredients

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { loadShoppingList } from '../store/shopping-list.actions';
+import { IShoppingListItem } from '../store/shopping-list.state';
 
 @Component({
   selector: 'app-shopping-list',
@@ -8,7 +9,7 @@ import { loadShoppingList } from '../store/shopping-list.actions';
   styleUrl: './shopping-list.component.scss',
 })
 export class ShoppingListComponent implements OnInit{
-  @Input() ingredients?: any[];
+  @Input() ingredients?: IShoppingListItem[];
 
   constructor(private store: Store) {}
 
